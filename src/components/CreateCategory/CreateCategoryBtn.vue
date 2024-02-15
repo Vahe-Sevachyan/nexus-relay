0d91142bd3040d1b252b97069298fba80e7ce7a2
Vahe Sevachyan <vscode13@gmail.com>
2024-02-14 19:39:56 -0800

    created CategoryBtn Component

---
+++ src/components/CreateCategory/CreateCategoryBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="btn-container">
+      <h3>Create Category</h3>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+<script></script>
+<style scoped>
+.container {
+  background-color: grey;
+  width: 17rem;
+}
+.btn-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 11rem;
+  margin: auto;
+}
+button {
+  height: 25px;
+}
+</style>
---
+++ src/components/CreateCategory/CreateCategoryContainer.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CreateCategoryBtn from "./CreateCategoryBtn.vue";
+</script>
+
+<template>
+  <CreateCategoryBtn />
+</template>
+
+<style scoped></style>
